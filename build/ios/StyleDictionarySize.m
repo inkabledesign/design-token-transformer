b4ad4f4aa6b790ee433201f3056680414204f202
Automated Publisher <actions@users.noreply.github.com>
2021-01-13 17:29:37 +0000

    Automated publish: Wed Jan 13 17:29:37 UTC 2021 13bf1a33ff46df3af5dda2d61eea446eddf55f9b

--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -3,7 +3,7 @@
 // StyleDictionarySize.m
 //
 // Do not edit directly
-// Generated on Wed, 13 Jan 2021 16:53:34 GMT
+// Generated on Wed, 13 Jan 2021 17:29:36 GMT
 //
 
 #import "StyleDictionarySize.h"

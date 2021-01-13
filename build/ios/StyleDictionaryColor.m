a1a713c758dddd5a35d81ee1a742eec36a6261cd
Automated Publisher <actions@users.noreply.github.com>
2021-01-13 09:26:10 +0000

    Automated publish: Wed Jan 13 09:26:10 UTC 2021 369cee390be93b48861dfdbbf27b2ef3e528b5b2

---
+++ build/ios/StyleDictionaryColor.m
@@ -0,0 +1,64 @@
+
+//
+// StyleDictionaryColor.m
+//
+// Do not edit directly
+// Generated on Wed, 13 Jan 2021 09:26:09 GMT
+//
+
+#import "StyleDictionaryColor.h"
+
+
+@implementation StyleDictionaryColor
+
++ (UIColor *)color:(StyleDictionaryColorName)colorEnum{
+  return [[self values] objectAtIndex:colorEnum];
+}
+
++ (NSArray *)values {
+  static NSArray* colorArray;
+  static dispatch_once_t onceToken;
+
+  dispatch_once(&onceToken, ^{
+    colorArray = @[
+rgba(255, 230, 0, 1),
+rgba(112, 141, 242, 1),
+rgba(4, 74, 255, 1),
+rgba(64, 255, 186, 1),
+rgba(64, 255, 186, 1),
+rgba(0, 0, 0, 0.1),
+rgba(4, 74, 255, 1),
+rgba(64, 223, 80, 1),
+rgba(52, 86, 175, 1),
+rgba(255, 255, 255, 0.06),
+rgba(255, 255, 255, 0.24),
+rgba(255, 255, 255, 1),
+rgba(255, 184, 0, 1),
+rgba(255, 184, 0, 1),
+rgba(255, 255, 255, 1),
+rgba(255, 255, 255, 1),
+rgba(207, 48, 48, 1),
+rgba(255, 255, 255, 1),
+rgba(74, 79, 204, 1),
+rgba(255, 255, 255, 1),
+rgba(255, 184, 0, 1),
+rgba(255, 138, 0, 1),
+rgba(255, 46, 0, 1),
+rgba(255, 0, 0, 1),
+rgba(0, 0, 0, 0.25),
+rgba(0, 0, 0, 0.25),
+rgba(0, 0, 0, 1),
+rgba(0, 0, 0, 0.25),
+rgba(0, 0, 0, 0.1),
+rgba(0, 0, 0, 0.06),
+rgba(0, 0, 0, 0.05),
+rgba(0, 0, 0, 0.04),
+rgba(0, 0, 0, 0.04),
+rgba(0, 0, 0, 0.1)
+    ];
+  });
+
+  return colorArray;
+}
+
+@end
